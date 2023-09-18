11996ef74e4dd9e0a009297632d1646b81aef017
IfLostart2per5 <121456361+IfLostart2per5@users.noreply.github.com>
2023-09-18 19:57:52 -0300

    Some changes
    1. Added a INIT_ARRAY macro
    2. increased the memory error support
    3. Refactored the err_message function
    4. Added some comments

--- interpreter.cpp
+++ interpreter.cpp
@@ -1,32 +1,41 @@
 #include <vector>
 #include <iostream>
+//macro util ;)
+#define INIT_ARRAY(type, name, size, value) type name[size]; \
+  for (size_t i = 0; i < size; i++) \
+    name[i] = value;
+
+void err_message(int type, int idx, int j, int x, int y) {
+    if (type == 1) 
+      std::cerr << "ERR (at instruction index " << idx << "): Index " << j << " is out of range. ignoring instructions until a recovery-point (32 instruction)" << std::endl;
+    else if (type == 2)
+      std::cerr << "ERR (at instruction index " << idx << "): Stack indexers " << x << " and " << y << " is out of range. Ignoring instructions until a recovery-point (32 instruction)" << std::endl;
+    else if (type == 3)
+      std::cerr << "ERR (at instruction index " << idx << " to " << idx + 2 << "): invalid memory set. Ignoring instructions until a recovery-point (32 instruction) (tip: the memory size is just 100 slots ;) )" << std::endl;
+    else if (type == 4)
+      std::cerr << "ERR: (at instruction index " << idx << " to " << idx + 1 << "): invalid memory get. Ignoring instructions until a recovery-point (32 instruction). (tip: the memory size is just 100 slots ;) )" << std::endl;
+    else if (type == 5)
+      std::cerr << "ERR: (at instruction index " << idx << "): division by zero not allowed!. Ignoring instructions until a revovery-poimt (32 instruction)." << std::endl;
+    else if (type == 6)
+      std::cerr << "ERR: (at instruction index " << idx << "): invalid non-initialized memory access. Ignoring instructions until a recovery-point (32 instruction)." << std::endl;
+  }
 
 
  std::vector<int> interpret(int values[], int valsize, int instructions[],int size, bool autoinc) {
   std::vector<int> stack;
+
+
   static int memory[100];
+  static INIT_ARRAY(bool, memory_marking, 100, false);
+  //indices rastreadores, j é o indice dos valores,e x e y são os indices da pilha
   int j = 0;
   int x = 0;
   int y = 1;
   bool errOcurred = false;
 
 
-  auto err_message = [&](int type, int idx) {
-    if (type == 1) 
-      std::cout << "ERR (at instruction index " << idx << "): Index " << j << " is out of range. ignoring instructions until a recovery-point (32 instruction)" << std::endl;
-    else if (type == 2)
-      std::cout << "ERR (at instruction index " << idx << "): Stack indexers " << x << " and " << y << " is out of range. Ignoring instructions until a recovery-point (32 instruction)" << std::endl;
-    else if (type == 3)
-      std::cout << "ERR (at instruction index " << idx << " to " << idx + 2 << "): invalid memory set. Ignoring instructions until a recovery-point (32 instruction) (tip: the memory size is just 100 slots ;) )" << std::endl;
-    else if (type == 4)
-      std::cout << "ERR: (at instruction index " << idx << " to " << idx + 1 << "): invalid memory get. Ignoring instructions until a recovery-point (32 instruction). (tip: the memory size is just 100 slots ;) )" << std::endl;
-    else if (type == 5)
-      std::cout << "ERR: (at instruction index " << idx << "): division by zero not allowed!. Ignoring instructions until a revovery-poimt (32 instruction)." << std::endl;
-
-  };
-
-
   for (int i = 0; i < size; i++) {
+    //verifica se ja ocorreu um erro, se sim,entra em pânico
     if (errOcurred) {
       if (instructions[i] != 32) {
         
@@ -46,7 +55,7 @@
           if (autoinc)
             j += 1;
         } else {
-          err_message(1, i);
+          err_message(1, i, j, x, y);
           errOcurred = true;
         }
 
@@ -57,26 +66,31 @@
           if (autoinc)
             j -= 1;
         } else {
-          err_message(1, i);
+          err_message(1, i, j, x, y);
           errOcurred = true;
         }
 
         break;
       case 3:
+        //aqui ele faz algumas otimizações aritméticas simples, valendo pros outros operadores também
         if(x < stack.size() && y < stack.size()) {
           if (!(stack[x] == 0 && stack[y] == 0)) {
+
             stack[x] += stack[y];
             stack.erase(stack.begin() + y);
           } else if (stack[x] == 0 && stack[y] != 0) {
+
             stack[x] = stack[y];
             stack.erase(stack.begin() + y);
           } else if (stack[y] == 0 && stack[x] != 0) {
+
             stack.erase(stack.begin() + y);
           } else {
+
             stack.erase(stack.begin() + y);
           }
         } else {
-          err_message(2, i);
+          err_message(2, i, j, x, y);
           errOcurred = true;
         }
         break;
@@ -94,7 +108,7 @@
             stack.erase(stack.begin() + y);
           }
         } else {
-          err_message(2, i);
+          err_message(2, i, j, x, y);
           errOcurred = true;
         }
         break;
@@ -107,14 +121,14 @@
         stack[x] -= stack[y];
         stack.erase(stack.begin() + y);
         } else {
-          err_message(2, i);
+          err_message(2, i, j, x, y);
           errOcurred = true;
         }
         break;
       case 6:
         if (x < stack.size() && y < stack.size()) {
           if (stack[y] == 0) {
-            err_message(5, i);
+            err_message(5, i, j, x, y);
             errOcurred = true;
             break;
           }
@@ -129,7 +143,7 @@
         stack[x] /= stack[y];
         stack.erase(stack.begin() + y);
         } else {
-          err_message(2, i);
+          err_message(2, i, j, x, y);
           errOcurred = true;
         };
         break;
@@ -143,12 +157,13 @@
         x -= 1;
         y -= 1;
         break;
+      //TODO: otimizar operações bit a bit
       case 9:
         if (x < stack.size() && y < stack.size()) {
         stack[x] &= stack[y];
         stack.erase(stack.begin() + y);
         } else {
-          err_message(2, i);
+          err_message(2, i, j, x, y);
           errOcurred = true;
         }
         break;
@@ -157,7 +172,7 @@
         stack[x] |= stack[y];
         stack.erase(stack.begin() + y);
         } else {
-          err_message(2, i);
+          err_message(2, i, j, x, y);
           errOcurred = true;
         }
         break;
@@ -165,7 +180,7 @@
         if (y < stack.size()) {
           stack[y] = ~stack[y];
         } else {
-         err_message(2, i);
+         err_message(2, i, j, x, y);
          errOcurred = true;
         }
         break;
@@ -181,22 +196,46 @@
         break;
       case 14:
         if (instructions[i + 1] < 100) {
-        memory[instructions[i + 1]] = instructions[i + 2];
-        i += 2;
+          memory[instructions[i + 1]] = instructions[i + 2];
+          memory_marking[instructions[i + 1]] = true;
+          i += 2;
         } else {
-          err_message(3, i);
+          err_message(3, i, j, x, y);
           errOcurred = true;
        
         }
         break;
       case 15:
         if (instructions[i + 1] < 100) {
-        stack.push_back(memory[instructions[i + 1]]);
-        i += 1;
+          if (memory_marking[instructions[i + 1]]) {
+            stack.push_back(memory[instructions[i + 1]]);
+            i += 1;
+          } else {
+            err_message(6, i, j, x, y);
+            errOcurred = true;
+          }
         } else {
-          err_message(4, i);
+          err_message(4, i, j, x, y);
           errOcurred = true;
         }
+        break;
+      case 16:
+        if (instructions[i + 1] < 100) {
+          if (memory_marking[instructions[i + 1]]) {
+
+            memory[instructions[i + 1]] = (int) NULL;
+            memory_marking[instructions[i + 1]] = false;
+            i += 1;
+          } else {
+
+            err_message(6, i, j, x, y);
+            errOcurred = true;
+          } 
+        } else {
+          err_message(4, i, j, x, y);
+          errOcurred = true;
+        }
+        
         break;
 
     }
